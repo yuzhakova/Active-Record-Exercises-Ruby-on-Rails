19ebb6a811305bb1914c344951d3806986cbf9c1
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2020-08-19 06:16:32 +0000

    Exercise 3: Delete the third store

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -1,3 +1,5 @@
+# bundle exec ruby exercises/exercise_3.rb
+
 require_relative '../setup'
 require_relative './exercise_1'
 require_relative './exercise_2'
@@ -6,3 +8,7 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+@store3 = Store.find_by(id: 3)
+@store3.destroy
+
+puts Store.count
