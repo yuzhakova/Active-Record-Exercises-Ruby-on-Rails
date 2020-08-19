73127bcc37726329633a10f74479d2921f9568db
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2020-08-19 06:58:01 +0000

    Exercise 6: One-to-many association

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -1,3 +1,4 @@
+# bundle exec ruby exercises/exercise_6.rb
 require_relative '../setup'
 require_relative './exercise_1'
 require_relative './exercise_2'
@@ -9,3 +10,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "B", last_name: "B", hourly_rate: 35)
+@store1.employees.create(first_name: "C", last_name: "C", hourly_rate: 40)
+@store1.employees.create(first_name: "D", last_name: "D", hourly_rate: 45)
+@store1.employees.create(first_name: "E", last_name: "E", hourly_rate: 55)
+@store2.employees.create(first_name: "F", last_name: "F", hourly_rate: 33)
