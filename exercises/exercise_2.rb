5548d5e8c095aa87eae5bad53642940f49b7002f
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2020-08-19 05:45:56 +0000

    Exercise 2: Update the first store

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -1,7 +1,11 @@
+# use this to run the file in CLI: bundle exec ruby exercises/exercise_2.rb
 require_relative '../setup'
 require_relative './exercise_1'
 
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+
+@store1.update(name: "First store")
