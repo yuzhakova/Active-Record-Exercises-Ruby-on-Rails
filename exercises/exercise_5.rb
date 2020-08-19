98ef765166db3dd69c808cb20de65e02ddfa58d2
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2020-08-19 06:38:13 +0000

    Exercise 5: Calculations

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -1,3 +1,4 @@
+# bundle exec ruby exercises/exercise_5.rb
 require_relative '../setup'
 require_relative './exercise_1'
 require_relative './exercise_2'
@@ -8,3 +9,8 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@company_revenue = Store.sum("annual_revenue")
+puts @company_revenue
+puts @company_revenue / Store.count
+
+puts Store.where("annual_revenue > 1000000").count
